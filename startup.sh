678bd0d158b1c4245cb13df7126737b46e3b1ea7
ideamac <ll@ll.c>
2024-03-11 20:34:51 +0800

    no message

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 
 #sudo rm -rf ../.cache ../.dbgpts
 kill -9 `ps aux | grep 'dbgpt/app/dbgpt_server.py' | grep -v 'grep' | awk -F ' ' '{print $2}'`
-nohup python dbgpt/app/dbgpt_server.py > ../log.log &
+nohup python dbgpt/app/dbgpt_server.py > ../tmp/log.log &
