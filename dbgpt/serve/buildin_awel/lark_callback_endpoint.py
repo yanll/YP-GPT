1a0151c6e72dee81a0b84ea99e2e83c023620942
ideamac <ll@ll.c>
2024-04-13 01:03:16 +0800

    .

--- dbgpt/serve/buildin_awel/lark_callback_endpoint.py
+++ dbgpt/serve/buildin_awel/lark_callback_endpoint.py
@@ -1,31 +1,24 @@
-from typing import Optional
+import json
+from typing import Dict
 
-from dbgpt._private.pydantic import BaseModel, Field
 from dbgpt.core.awel import DAG, HttpTrigger, MapOperator
 
 
-class ReqContext(BaseModel):
-    conv_uid: str = Field(..., description="会话标识")
-
-
-class TriggerReqBody(BaseModel):
-    pass
-
-
-class RequestHandleOperator(MapOperator[TriggerReqBody, str]):
+class RequestHandleOperator(MapOperator[Dict, str]):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-    async def map(self, input_body: TriggerReqBody) -> str:
+    async def map(self, input_body: Dict) -> str:
         print(f"Receive input body: {input_body}")
-        return "OK," + input_body
+        rs = "received message:" + json.dumps(input_body)
+        return rs
 
 
 with DAG("dbgpt_awel_lark_callback_endpoint") as dag:
     trigger = HttpTrigger(
         "/lark_callback_endpoint",
         methods="POST",
-        request_body=TriggerReqBody
+        request_body=Dict
     )
     map_node = RequestHandleOperator()
     trigger >> map_node
---
+++ dbgpt/util/tests/test_requests.py
@@ -0,0 +1,16 @@
+import requests
+import json
+
+
+def test_select_userinfo():
+    url = "http://localhost:3000/api/v1/awel/trigger/lark_callback_endpoint"
+    headers = {'Content-Type': 'application/json; charset=utf-8'}
+    data = {
+        "context": {
+            "conv_uid": "123456"
+        },
+        "message": "查询商户编号是10012982662的商户详细信息"
+    }
+    resp = requests.request('POST', headers=headers, url=url, data=json.dumps(data))
+    print("调用返回结果：", resp.text)
+    assert True
