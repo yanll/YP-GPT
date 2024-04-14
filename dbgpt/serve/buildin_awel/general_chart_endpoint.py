15dfb62d397ed4f30c54a864894bea4fcaf56bea
ideamac <ll@ll.c>
2024-04-14 23:45:40 +0800

    .

--- dbgpt/serve/buildin_awel/general_chart_endpoint.py
+++ dbgpt/serve/buildin_awel/general_chart_endpoint.py
@@ -6,6 +6,9 @@ from langchain_core.prompts import PromptTemplate
 from langchain_openai import AzureChatOpenAI
 
 from dbgpt.core.awel import DAG, HttpTrigger, MapOperator
+from langchain_core.messages import HumanMessage
+from langchain_openai import AzureChatOpenAI
+from langgraph.graph import END, MessageGraph
 
 
 class RequestHandleOperator(MapOperator[Dict, str]):
@@ -29,8 +32,20 @@ class RequestHandleOperator(MapOperator[Dict, str]):
             template="{msg}",
             input_variables=["msg"]
         )
-        chain = LLMChain(llm=self.llm, prompt=prompt)
-        return chain.invoke({"msg": "hello, who are you?"})
+        # chain = LLMChain(llm=self.llm, prompt=prompt)
+        # return chain.invoke({"msg": "hello, who are you?"})
+
+        graph = MessageGraph()
+
+        graph.add_node("oracle", self.llm)
+        graph.add_edge("oracle", END)
+
+        graph.set_entry_point("oracle")
+
+        runnable = graph.compile()
+
+        rs = runnable.invoke(HumanMessage("What is 1 + 1?"))
+        return rs
 
 
 with DAG("dbgpt_awel_general_chat_endpoint") as dag:
