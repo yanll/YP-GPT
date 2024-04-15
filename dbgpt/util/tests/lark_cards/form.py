31dd3325063800f83d25a998c6158f97fcfa8a7a
ideamac <ll@ll.c>
2024-04-15 15:58:42 +0800

    .

--- dbgpt/util/tests/lark_cards/form.py
+++ dbgpt/util/tests/lark_cards/form.py
@@ -5,7 +5,7 @@ def test_form_template() -> Dict:
     return {
         "type": "template",
         "data": {
-            "template_id": "AAqkwmwOTohjy", "template_version_name": "1.0.1",
+            "template_id": "AAqkwmwOTohjy", "template_version_name": "1.0.2",
             "template_variable": {
                 "ai_message": "请提供完整的信息！"
             }
