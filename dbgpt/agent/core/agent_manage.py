dbe22ac9b06287bf4fd2d4b955a567c82f6fca56
ideamac <ll@ll.c>
2024-04-15 15:36:52 +0800

    .

--- dbgpt/agent/core/agent_manage.py
+++ dbgpt/agent/core/agent_manage.py
@@ -70,6 +70,8 @@ class AgentManager(BaseComponent):
         from ..expand.data_scientist_agent import DataScientistAgent
         from ..expand.plugin_assistant_agent import PluginAssistantAgent
         from ..expand.summary_assistant_agent import SummaryAssistantAgent
+        from ..expand.custom.calendar_assistant_agent import CalendarAssistantAgent
+        from ..expand.custom.demand_collect_agent import ProductionAssistantAgent
 
         core_agents = set()
         core_agents.add(self.register_agent(CodeAssistantAgent))
@@ -77,6 +79,8 @@ class AgentManager(BaseComponent):
         core_agents.add(self.register_agent(DataScientistAgent))
         core_agents.add(self.register_agent(SummaryAssistantAgent))
         core_agents.add(self.register_agent(PluginAssistantAgent))
+        core_agents.add(self.register_agent(CalendarAssistantAgent))
+        core_agents.add(self.register_agent(ProductionAssistantAgent))
         self._core_agents = core_agents
 
     def register_agent(
