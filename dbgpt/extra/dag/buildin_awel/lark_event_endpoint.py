5dcb0ed16cf296ca75533d3532303a47903098b9
ideamac <ll@ll.c>
2024-04-17 21:10:06 +0800

    .

--- dbgpt/extra/dag/buildin_awel/lark_event_endpoint.py
+++ dbgpt/extra/dag/buildin_awel/lark_event_endpoint.py
@@ -1,30 +1,56 @@
 import asyncio
 import json
 import logging
-from typing import Dict
+from typing import Dict, List
+import re
+from langchain_core.messages import HumanMessage
+from langgraph.graph import END, MessageGraph
 
-from langchain.chains.llm import LLMChain
-from langchain_core.prompts import PromptTemplate
-
+from dbgpt.client import Client
 from dbgpt.core.awel import DAG, HttpTrigger, MapOperator
+from dbgpt.core.schema.api import ChatCompletionResponse
+from dbgpt.extra.dag.buildin_awel.app.service import GptsAppService
+from dbgpt.storage.chat_history import ChatHistoryMessageEntity
+from dbgpt.storage.chat_history.chat_history_db import ChatHistoryMessageDao
 from dbgpt.util import larkutil
 from dbgpt.util.azure_util import create_azure_llm
+
+
+class InitAppsOperator(MapOperator[Dict, str]):
+
+    def __init__(self, **kwargs):
+        self.apps = []
+        self.gpts_app_service = GptsAppService()
+        super().__init__(**kwargs)
+
+    async def map(self, input_body: Dict):
+        try:
+            print(f"开始加载应用列表: ", input_body)
+            apps = self.gpts_app_service.get_gpts_app_list("singe_agent")
+            self.apps = apps
+            input_body['apps'] = self.apps
+            return input_body
+        except Exception as e:
+            logging.exception("加载应用列表异常", e)
+            return input_body
 
 
 class RequestHandleOperator(MapOperator[Dict, str]):
     llm = None
 
     def __init__(self, **kwargs):
+        self.chat_history_message_dao = ChatHistoryMessageDao()
         self.llm = create_azure_llm()
         super().__init__(**kwargs)
 
     async def map(self, input_body: Dict) -> str:
         try:
-            print(f"Receive input body: {input_body}")
+            print(f"接收飞书事件: {input_body}")
             # 首次验证挑战码
             if "challenge" in input_body:
                 return {"challenge": input_body["challenge"]}
 
+            apps = input_body["apps"]
             header = input_body["header"]
             event = input_body["event"]
             sender_open_id = event["sender"]["sender_id"]["open_id"]
@@ -34,9 +60,10 @@ class RequestHandleOperator(MapOperator[Dict, str]):
             content = json.loads(message["content"])
             content_text = content["text"]
 
-
             if message_type == "text" and sender_open_id != "" and content_text != "" and chat_type == "p2p":
-                asyncio.create_task(handle(self.llm, sender_open_id, content_text))
+                asyncio.create_task(
+                    request_handle(apps, self.llm, self.chat_history_message_dao, sender_open_id, content_text)
+                )
             return {"message": "OK"}
         except Exception as e:
             logging.exception("飞书事件处理异常！", e)
@@ -49,21 +76,113 @@ with DAG("dbgpt_awel_lark_event_endpoint") as dag:
         methods="POST",
         request_body=Dict
     )
+
+    init_apps = InitAppsOperator()
     map_node = RequestHandleOperator()
-    trigger >> map_node
+    trigger >> init_apps >> map_node
 
 
-async def handle(llm, sender_open_id, human_message):
+async def request_handle(apps, llm, chat_history_dao: ChatHistoryMessageDao, sender_open_id, human_message):
     print("lark_event_endpoint async handle：", human_message)
-    prompt = PromptTemplate(
-        template="{msg}",
-        input_variables=["msg"]
+
+    graph = MessageGraph()
+    graph.add_node("call_extract_app", call_extract_app)
+    graph.add_edge("call_extract_app", END)
+    graph.set_entry_point("call_extract_app")
+    runnable = graph.compile()
+    messages = []
+    his: List[ChatHistoryMessageEntity] = chat_history_dao.get_his_messages_by_uid(sender_open_id)
+    his: List[ChatHistoryMessageEntity] = []
+    role_desc = (
+        f"你是一个智能助手，现在有以下应用：{json.dumps(apps)}\n"
+        "请根据我输入的内容识别我的意图，根据意图匹配需要使用哪个应用，意图匹配成功后按照：{'app_code': 'the value of app_code', 'app_descpibe': 'the value of app_descpibe'}格式回复给我，不要回复其他内容。\n"
+        "如果没有匹配成功，按照通用AI回复\n"
     )
-    chain = LLMChain(llm=llm, prompt=prompt)
-    ai_message = chain.invoke({"msg": human_message})
-    content = {"text": human_message + "：\n\n" + ai_message["text"]}
-    larkutil.send_message(
-        receive_id=sender_open_id,
-        content=content,
-        receive_id_type="open_id"
+    "以下是用户和AI的对话内容：\n"
+    "\n\n"
+
+    messages.append(HumanMessage(name=sender_open_id, content="human:" + role_desc))
+    if his:
+        for m in his:
+            dict = json.loads(m.message_detail)
+            if dict["type"] == "human":
+                messages.append(HumanMessage(name=sender_open_id, content="human:" + dict["data"]["content"]))
+            if dict["type"] == "ai":
+                messages.append(HumanMessage(name=sender_open_id, content="ai:" + dict["data"]["content"]))
+    messages.append(HumanMessage(name=sender_open_id, content="human:" + human_message))
+    await runnable.ainvoke(messages)
+
+
+async def call_extract_app(messages: List[HumanMessage]):
+    DBGPT_API_KEY = ""
+    client = Client(api_key=DBGPT_API_KEY)
+    mess = []
+    conv_uid = messages[0].name
+    for m in messages:
+        mess.append(m.content + "\n")
+    res: ChatCompletionResponse = await client.chat(
+        model="proxyllm",
+        messages="".join(mess),
+        conv_uid=conv_uid
     )
+    ai_message = res.choices[0].message.content
+    print("路由识别结果：", ai_message)
+    match = re.search(r"\{.*?\}", ai_message)
+    response_text = ""
+    strutured_message = None
+    if match:
+        strutured_message = match.group()
+    print("路由解析结果：", strutured_message)
+    if strutured_message:
+        dic = json.loads(strutured_message.replace("'", "\""))
+        app_code = dic["app_code"]
+
+        async for data in client.chat_stream(
+                messages=messages[-1].content.replace("human:", ""),
+                model="proxyllm",
+                chat_mode="chat_app",
+                chat_param=app_code,
+                conv_uid=conv_uid
+
+        ):
+            try:
+                content = data.choices[0].delta.content
+                agent_messages_json = content.split("```agent-messages\\n")[1].split("\\n```")[0]
+                agent_messages_json = agent_messages_json.replace("\\\"", "\"")
+                agent_messages = json.loads(agent_messages_json)
+                markdown_text = agent_messages[0]['markdown']
+                response_text = markdown_text
+                print("循环响应结果：", response_text)
+            except Exception as e:
+                print("Error extracting markdown text:", str(e))
+                continue
+
+        larkutil.send_message(
+            receive_id=conv_uid,
+            content={"text": response_text},
+            receive_id_type="open_id"
+        )
+
+    # if (ai_message.startswith("{'app_code'")):
+    #     larkutil.send_message(
+    #         receive_id=conv_uid,
+    #         content={
+    #             "type": "template",
+    #             "data": {
+    #                 "template_id": "AAqkwmwOTohjy", "template_version_name": "1.0.10",
+    #                 "template_variable": {
+    #                     "ai_message": "请提供完整的信息！"
+    #                 }
+    #             }
+    #         },
+    #         receive_id_type="open_id",
+    #         msg_type="interactive"
+    #     )
+
+    else:
+        larkutil.send_message(
+            receive_id=conv_uid,
+            content={"text": ai_message.replace("AI: ", "")},
+            receive_id_type="open_id"
+        )
+    return res
