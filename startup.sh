5718d562560fbff8c3ccc2e8c01029c6ba612dba
ideamac <ll@ll.c>
2024-04-17 23:47:27 +0800

    .

--- startup.sh
+++ startup.sh
@@ -1,4 +1,8 @@
 
 #sudo rm -rf ../.cache ../.dbgpts
+
+conda activate dbgpt_env
 kill -9 `ps aux | grep 'dbgpt/app/dbgpt_server.py' | grep -v 'grep' | awk -F ' ' '{print $2}'`
+sudo rm -rf ../tmp/log.log &
 nohup python dbgpt/app/dbgpt_server.py --port 5670 > ../tmp/log.log &
+tail -f ../tmp/log.log
