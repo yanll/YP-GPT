9eb53148731dccea9d230902727820264709a1a0
ideamac <ll@ll.c>
2024-04-17 22:14:47 +0800

    .

--- dbgpt/extra/dag/buildin_awel/lark_event_endpoint.py
+++ dbgpt/extra/dag/buildin_awel/lark_event_endpoint.py
@@ -95,9 +95,10 @@ async def request_handle(apps, llm, chat_history_dao: ChatHistoryMessageDao, sen
     his: List[ChatHistoryMessageEntity] = chat_history_dao.get_his_messages_by_uid(sender_open_id)
     his: List[ChatHistoryMessageEntity] = []
     role_desc = (
-        f"你是一个智能助手，现在有以下应用：{json.dumps(apps)}\n"
-        "请根据我输入的内容识别我的意图，根据意图匹配需要使用哪个应用，如果意图匹配成功则按照：{'app_code': 'the value of app_code', 'app_descpibe': 'the value of app_descpibe'}格式回复给我，不要回复其他内容。\n"
-        "如果没有匹配成功则按照通用AI角色回复我的问题\n"
+        f"你是一个智能助手，现在有以下应用：\n"
+        f"{json.dumps(apps)}\n"
+        "请根据我输入的内容识别我的意图，根据意图匹配需要使用哪个应用，如果意图匹配成功则按照：{'app_code': 'the value of app_code', 'app_descpibe': 'the value of app_descpibe'}格式回复给我（不要回复多余内容），否则按照通用AI角色回复我的问题。\n"
+        "\n"
     )
     "以下是我和AI的对话内容：\n"
     "\n\n"
