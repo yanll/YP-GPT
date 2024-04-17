8ddaeef980bec70b70a2bf021281564c01de334d
ideamac <ll@ll.c>
2024-04-18 00:48:44 +0800

    .

--- startup.sh
+++ startup.sh
@@ -5,4 +5,3 @@
 kill -9 `ps aux | grep 'dbgpt/app/dbgpt_server.py' | grep -v 'grep' | awk -F ' ' '{print $2}'`
 sudo rm -rf ../tmp/log.log
 nohup python dbgpt/app/dbgpt_server.py --port 5670 > ../tmp/log.log &
-tail -f ../tmp/log.log
