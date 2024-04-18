3fe123c75c954d5bd0941ee480432b904a7f221a
ideamac <ll@ll.c>
2024-04-18 15:04:08 +0800

    .

--- dbgpt/extra/dag/buildin_awel/app/gpts_app_db.py
+++ dbgpt/extra/dag/buildin_awel/app/gpts_app_db.py
@@ -10,7 +10,7 @@ class GptsAppDao(BaseDao):
 
     def get_gpts_app_list(self, team_mode: str = None) -> List:
         session = self.get_raw_session()
-        result = session.execute(text("SELECT * FROM GPTS_APP where team_mode = :team_mode"), {'team_mode': team_mode})
+        result = session.execute(text("SELECT * FROM GPTS_APP where team_mode = :team_mode and (app_name like '%需求收集助手%' or app_name like '%会议室预定助手%')"), {'team_mode': team_mode})
         rs = []
         for row in result:
             dic = row._asdict()
