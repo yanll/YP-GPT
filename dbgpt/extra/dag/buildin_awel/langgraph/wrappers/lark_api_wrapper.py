40a6423114a8a27a3eeeb43dc40bf34ecbb0e063
ideamac <ll@ll.c>
2024-04-24 15:33:00 +0800

    .

--- dbgpt/extra/dag/buildin_awel/langgraph/wrappers/lark_api_wrapper.py
+++ dbgpt/extra/dag/buildin_awel/langgraph/wrappers/lark_api_wrapper.py
@@ -6,9 +6,15 @@ import datetime
 
 
 def handle_lark_callback(input_body: Dict):
-    form_value = input_body['event']['action']['form_value']
-
+    event = input_body['event']
+    operator = event['operator']
+    action = event['action']
+    form_value = action['form_value']
+    open_id = operator['open_id']
+    union_id = operator['union_id']
+    # open_id or union_id
     result = create_and_send_work_item(
+        union_id=open_id,
         name=form_value['requirement_content'],
         priority_value=form_value['emergency_level'],
         expected_time=form_value['expected_completion_date']
@@ -47,18 +53,39 @@ def get_project_app_token():
     data = {
         "plugin_id": "MII_6620D4D5830B401C",
         "plugin_secret": "249E74635DFA34E434B14EF3D7CA164D",
-        "type": 1
+        "type": 0
     }
     response = requests.post(url, headers=headers, data=json.dumps(data))
     return response.json()["data"]["token"]
 
 
-def create_and_send_work_item(name, priority_value, expected_time):
+def get_user_key(union_id):
+    url = 'https://project.feishu.cn/open_api/user/query'
+    headers = {
+        'X-PLUGIN-TOKEN': get_project_app_token(),
+        'Content-Type': 'application/json'
+    }
+    data = {
+        "out_ids": [union_id]
+    }
+
+    try:
+        response = requests.post(url, headers=headers, data=json.dumps(data))
+        response_data = response.json()
+        if response_data and 'data' in response_data and len(response_data['data']) > 0:
+            return response_data['data'][0].get('user_key')
+        else:
+            return "No user key found in the response."
+    except Exception as e:
+        return str(e)
+
+
+def create_and_send_work_item(union_id, name, priority_value, expected_time):
     # 直接在函数内定义 API URL 和 headers
     url = 'https://project.feishu.cn/open_api/ypgptapi/work_item/create'
     headers = {
         'X-PLUGIN-TOKEN': get_project_app_token(),
-        'X-USER-KEY': '7355229159460749315',
+        'X-USER-KEY': get_user_key(union_id),
         'X-IDEM-UUID': '',
         'Content-Type': 'application/json'
     }
