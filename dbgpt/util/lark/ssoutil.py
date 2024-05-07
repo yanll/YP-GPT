df026f2f9dce099c37c75561b4e7f7461fea1aee
ideamac <ll@ll.c>
2024-05-07 21:39:20 +0800

    .

--- dbgpt/util/lark/ssoutil.py
+++ dbgpt/util/lark/ssoutil.py
@@ -6,11 +6,16 @@ import requests
 
 from dbgpt.extra.cache.redis_cli import RedisClient
 from dbgpt.util import envutils
+from dbgpt.util.lark import larkutil
 
 redis_client = RedisClient()
 
 
 def get_sso_credential(open_id: str):
+
+    if True:
+        return envutils.getenv("SSO_TOKEN")
+
     url = envutils.getenv("PROC_CONSOLE_ENDPOINT") + '/auth/agent?openId=' + open_id
 
     credential = ""
@@ -23,6 +28,7 @@ def get_sso_credential(open_id: str):
         print("用户凭证信息缓存读取成功！", open_id)
         return credential
     else:
+        larkutil.select_userinfo(open_id=open_id)
         resp = requests.request(method='POST', url=url, headers={}, params={}, data={})
         if resp.status_code != 200:
             logging.error("用户凭证接口异常：" + str(resp.status_code))
