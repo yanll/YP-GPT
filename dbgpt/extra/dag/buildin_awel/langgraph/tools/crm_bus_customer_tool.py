e769d3aaa2a0c01efc74e13b8a42783a7319a289
ideamac <ll@ll.c>
2024-05-09 18:06:56 +0800

    .

--- dbgpt/extra/dag/buildin_awel/langgraph/tools/crm_bus_customer_tool.py
+++ dbgpt/extra/dag/buildin_awel/langgraph/tools/crm_bus_customer_tool.py
@@ -9,7 +9,7 @@ from langchain_core.callbacks import (
 from pydantic import BaseModel, Field
 
 from dbgpt.extra.dag.buildin_awel.lark import card_templates
-from dbgpt.util.lark import larkutil, lark_card_util
+from dbgpt.util.lark import lark_message_util, lark_card_util
 
 
 class CrmBusCustomerCollectInput(BaseModel):
@@ -129,7 +129,7 @@ def do_collect(
         """
         print("发送飞书提报卡片：", conv_id)
         if industry_line == "Web3.0行业线":
-            larkutil.send_message(
+            lark_message_util.send_message(
                 receive_id=conv_id,
                 content=card_templates.create_crm_bus_customer_card_content.Web3(
                     template_variable={
@@ -161,7 +161,7 @@ def do_collect(
                 msg_type="interactive"
             )
         else:
-            larkutil.send_message(
+            lark_message_util.send_message(
                 receive_id=conv_id,
                 content=card_templates.create_crm_bus_customer_card_content.Finance(
                     template_variable={
