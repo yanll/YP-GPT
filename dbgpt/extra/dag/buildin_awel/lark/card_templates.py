a8f0182a4b7faddadba1932337050c236ba24369
ideamac <ll@ll.c>
2024-05-11 19:22:03 +0800

    .

--- dbgpt/extra/dag/buildin_awel/lark/card_templates.py
+++ dbgpt/extra/dag/buildin_awel/lark/card_templates.py
@@ -31,7 +31,7 @@ def create_requirement_card_content(template_variable: Dict):
 def create_daily_report_card_content(template_variable: Dict):
     """日报表单"""
     template_id = "AAqkjM4Ffisl2"
-    template_version_name = "1.0.16"
+    template_version_name = "1.0.17"
 
     card = {
         "type": "template",
@@ -134,7 +134,7 @@ def create_interactive_update_daily_report_card_content(template_variable: Dict)
 def create_weekly_report_card_content(template_variable: Dict):
     """周报表单  """
     template_id = "AAqkjMz1cWwRB"
-    template_version_name = "1.0.10"
+    template_version_name = "1.0.13"
 
     card = {
         "type": "template",
