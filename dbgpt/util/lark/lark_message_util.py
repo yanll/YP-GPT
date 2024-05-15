0cdbe9df1176af99765c9ee7d7c8e15a8c17a0cb
bo.liu-2 <c0022@C0022deMacBook-Air.local>
2024-05-15 16:17:57 +0800

    fix: loading message

--- dbgpt/util/lark/lark_message_util.py
+++ dbgpt/util/lark/lark_message_util.py
@@ -62,7 +62,7 @@ def send_card_message_rag(receive_id: str, content: Dict):
 
 
 
-# 发送交互更新卡片
+# 发送应用发送的卡片消息
 def send_interactive_update_message(token: str, content: Dict):
     url = 'https://open.feishu.cn/open-apis/interactive/v1/card/update'
     data = {
@@ -73,3 +73,173 @@ def send_interactive_update_message(token: str, content: Dict):
                             timeout=consts.request_time_out)
     print('发送交互更新卡片返回结果：', resp.json())
     return resp.json()
+
+
+# 发送交互更新卡片
+def update_interactive_card_rag(message_id, content: Dict):
+    url = "https://open.feishu.cn/open-apis/im/v1/messages/{}".format(message_id)
+    data = {
+        "content": json.dumps(content)
+    }
+    
+    resp = requests.patch(url=url, headers=build_headers_rag(), json=data)
+    
+    print('发送交互更新卡片返回结果：', resp.json())
+    return resp.json()
+
+
+def send_loading_message_rag(receive_id):
+    resp_data = send_message_rag(receive_id=receive_id,content=resp_loading_hint,receive_id_type='open_id',msg_type='interactive',type='card_message')
+    return resp_data['message_id']
+
+def update_loading_message_rag(message_id,type='standard'):
+    resp_placeholder = None
+    if type == 'standard':
+        resp_placeholder = resp_standard_hint
+    elif type == 'error':
+        resp_placeholder = resp_error_hint
+    res = update_interactive_card_rag(message_id=message_id,content=resp_placeholder)
+    return res
+
+
+
+resp_loading_hint = {
+    "config": {},
+    "i18n_elements": {
+        "zh_cn": [
+            {
+                "tag": "column_set",
+                "flex_mode": "none",
+                "background_style": "default",
+                "horizontal_spacing": "4px",
+                "horizontal_align": "center",
+                "columns": [
+                    {
+                        "tag": "column",
+                        "width": "30px",
+                        "vertical_align": "center",
+                        "background_style": "default",
+                        "elements": [
+                            {
+                                "tag": "img",
+                                "img_key": "img_v3_02at_d325ba34-7bb2-4b80-977b-8092ebaf29dg",
+                                "preview": False,
+                                "transparent": True,
+                                "scale_type": "crop_center",
+                                "size": "stretch",
+                                "alt": {
+                                    "tag": "plain_text",
+                                    "content": ""
+                                }
+                            }
+                        ]
+                    },
+                    {
+                        "tag": "column",
+                        "width": "weighted",
+                        "vertical_align": "center",
+                        "background_style": "default",
+                        "elements": [
+                            {
+                                "tag": "column_set",
+                                "flex_mode": "none",
+                                "horizontal_spacing": "default",
+                                "background_style": "default",
+                                "columns": [
+                                    {
+                                        "tag": "column",
+                                        "elements": [
+                                            {
+                                                "tag": "div",
+                                                "text": {
+                                                    "tag": "plain_text",
+                                                    "content": "正在使用企业知识库回答",
+                                                    "text_size": "normal",
+                                                    "text_align": "left",
+                                                    "text_color": "default"
+                                                }
+                                            }
+                                        ],
+                                        "width": "weighted",
+                                        "weight": 1
+                                    }
+                                ]
+                            }
+                        ],
+                        "weight": 1
+                    }
+                ],
+                "margin": "0px 0px 0px 0px"
+            }
+        ]
+    },
+    "i18n_header": {}
+}
+
+resp_standard_hint = {
+    "config": {},
+    "i18n_elements": {
+        "zh_cn": [
+            {
+                "tag": "column_set",
+                "flex_mode": "none",
+                "horizontal_spacing": "default",
+                "background_style": "default",
+                "columns": [
+                    {
+                        "tag": "column",
+                        "elements": [
+                            {
+                                "tag": "div",
+                                "text": {
+                                    "tag": "plain_text",
+                                    "content": "已经根据企业知识库生成答案！",
+                                    "text_size": "normal",
+                                    "text_align": "left",
+                                    "text_color": "default"
+                                }
+                            }
+                        ],
+                        "width": "weighted",
+                        "weight": 1
+                    }
+                ]
+            }
+        ]
+    },
+    "i18n_header": {}
+}
+
+resp_error_hint = {
+    "config": {},
+    "i18n_elements": {
+        "zh_cn": [
+            {
+                "tag": "column_set",
+                "flex_mode": "none",
+                "horizontal_spacing": "default",
+                "background_style": "default",
+                "columns": [
+                    {
+                        "tag": "column",
+                        "elements": [
+                            {
+                                "tag": "div",
+                                "text": {
+                                    "tag": "plain_text",
+                                    "content": "生成失败！",
+                                    "text_size": "normal",
+                                    "text_align": "left",
+                                    "text_color": "default"
+                                }
+                            }
+                        ],
+                        "width": "weighted",
+                        "weight": 1
+                    }
+                ]
+            }
+        ]
+    },
+    "i18n_header": {}
+}
