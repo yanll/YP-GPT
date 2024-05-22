243c5d1bc13483835fd009164fc8d9fc82622b48
zhx <zhx6066@126.com>
2024-05-22 21:25:21 +0800

    商户毛利情况图表更新

--- dbgpt/extra/dag/buildin_awel/langgraph/tools/tools_provider.py
+++ dbgpt/extra/dag/buildin_awel/langgraph/tools/tools_provider.py
@@ -4,6 +4,7 @@ from langchain.tools import BaseTool
 from langchain_core.utils.function_calling import convert_to_openai_tool
 from pydantic import BaseModel
 
+from dbgpt.extra.dag.buildin_awel.langgraph.tools.ability_display_tool import AbilityDisplayTool
 from dbgpt.extra.dag.buildin_awel.langgraph.tools.book_meeting_room_tool import BookMeetingRoomTool
 # from dbgpt.extra.dag.buildin_awel.langgraph.tools.ability_display_tool import AbilityDisplayTool
 from dbgpt.extra.dag.buildin_awel.langgraph.tools.crm_bus_customer_add_tool import CrmBusCustomerCollectAddTool
@@ -36,7 +37,7 @@ class ToolsProvider:
             CrmBusCustomerCollectQueryTool(),
             Dailypushmessagetool(),
             BookMeetingRoomTool(),
-            # AbilityDisplayTool(),
+            AbilityDisplayTool(),
             FeedbackCollectTool()
             # ,KnowledgeTool()
         ]
