bbc437ef17728ef4de69822ccf20f5252e1607fa
zhx <zhx6066@126.com>
2024-05-27 18:31:56 +0800

    商户毛利情况图表更新

--- dbgpt/extra/dag/buildin_awel/monitor/monitor3.py
+++ dbgpt/extra/dag/buildin_awel/monitor/monitor3.py
@@ -48,7 +48,9 @@ def deal_data(alert_list, d_1_data, d_2_data, customer_type):
                 alert_list.append({
                     'name': sale_name,
                     'title': '商户（收方或付方）产品波动异常',
-                    'content': f'{"商户签约名" if customer_type=="STAT_DISPAYSIGNEDNAME" else "付方签约名"}:{customer}，交易无明显波动，但{product}产品结构有变化，变化值为{difference1*100:.2f}%（产品交易金额占比的环比），请关注。'
+                    'type': '商户签约名',
+                    'customer_name': customer,
+                    'content': f'交易无明显波动，但{product}产品结构有变化，变化值为{difference1*100:.2f}%，请关注。'
                 })
 
     return alert_list
@@ -77,3 +79,5 @@ def monitor3():
     alert_list = deal_data(alert_list, d_1_data_by_payer, d_2_data_by_payer, 'PAYER_CUSTOMER_SIGNEDNAME')
 
     return alert_list
+a = monitor3()
+print(a)
