61b68000034bed00f0488fda47551826c4ddd9bb
zhx <zhx6066@126.com>
2024-05-27 10:36:14 +0800

    商户毛利情况图表更新

--- dbgpt/extra/dag/buildin_awel/lark/card_templates.py
+++ dbgpt/extra/dag/buildin_awel/lark/card_templates.py
@@ -393,7 +393,7 @@ def crem_sales_details_content(template_variable: Dict):
 def travel_report_content4(template_variable: Dict):
     """航旅波动检测归因-监控4"""
     template_id = "AAq37oHs6R1LR"
-    template_version_name = "1.0.11"
+    template_version_name = "1.0.14"
 
     card = {
         "type": "template",
