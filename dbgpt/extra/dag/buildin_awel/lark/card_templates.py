4ed03b67e7f7e5c879bf094be35906a93531c9a4
zhx <zhx6066@126.com>
2024-05-29 21:21:29 +0800

    .........

--- dbgpt/extra/dag/buildin_awel/lark/card_templates.py
+++ dbgpt/extra/dag/buildin_awel/lark/card_templates.py
@@ -393,7 +393,7 @@ def crem_sales_details_content(template_variable: Dict):
 def travel_report_content1(template_variable: Dict):
     """航旅波动检测归因-监控1"""
     template_id = "AAq3x4lBPjbC8"
-    template_version_name = "1.0.2"
+    template_version_name = "1.0.9"
 
     card = {
         "type": "template",
