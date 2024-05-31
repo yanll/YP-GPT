1d21ec48c92cf0d92a08c4822a11613c1572e7d9
ideamac <ll@ll.c>
2024-05-31 19:53:52 +0800

    监控4原始场景修改

--- dbgpt/extra/dag/buildin_awel/monitor/monitor4.py
+++ dbgpt/extra/dag/buildin_awel/monitor/monitor4.py
@@ -16,6 +16,8 @@ class Monitor4(AirlineMonitorDataHandler):
             # 自然日
             self.d_1_d_7_date = ','.join([(today - timedelta(days=x + 1)).strftime("%Y-%m-%d") for x in range(7)])
             self.d_8_d_14_date = ','.join([(today - timedelta(days=x + 8)).strftime("%Y-%m-%d") for x in range(7)])
+            self.original_scene_dict = self.get_original_scene_dict()
+
         except Exception as e:
             raise e
 
@@ -166,16 +168,20 @@ class Monitor4(AirlineMonitorDataHandler):
 
             for item2 in d_8_d_14_data:
                 for item1 in d_1_d_7_data:
-                    if item1['CUSTOMER_NO'] == item2['CUSTOMER_NO'] and item1['PAYER_BUSINESS_SCENE'] == item2[
-                        'PAYER_BUSINESS_SCENE']:
+                    if item1['CUSTOMER_NO'] == item2['CUSTOMER_NO']:
                         if float(item2["SUCCESS_AMOUNT"]) == 0 or d_8_d_14_customer_success_amount == 0:
                             continue
                         difference = float(item1["SUCCESS_AMOUNT"]) / float(item2[
                                                                                 "SUCCESS_AMOUNT"]) - d_1_d_7_customer_success_amount / d_8_d_14_customer_success_amount
+                        orig_scene = self.get_original_scene_by_merchant_no(
+                            self.original_scene_dict,
+                            item1["PAYER_BUSINESS_SCENE"]
+                        )
                         fluctuation = float(item1["SUCCESS_AMOUNT"]) / float(
                             item2["SUCCESS_AMOUNT"]) - self.market_fluctuation
-                        content = f'付方名称:{payer}，航司:{customer}——商编:{item1["CUSTOMER_NO"]}+场景字段:{item1["PAYER_BUSINESS_SCENE"]}，近7天充值金额，环比上周{"上升" if difference > 0 else "下降"}{abs(difference * 100):.2f}%，{"高于" if fluctuation > 0 else "低于"}大盘{abs(fluctuation * 100):.2f}%'
+                        content = f'付方名称:{payer}，航司:{customer}——商编:{item1["CUSTOMER_NO"]}+场景字段:{orig_scene}，近7天充值金额，环比上周{"上升" if difference > 0 else "下降"}{abs(difference * 100):.2f}%，{"高于" if fluctuation > 0 else "低于"}大盘{abs(fluctuation * 100):.2f}%'
                         subcontent = f"波动详情：  近7天充值金额，环比上周{'上升' if difference > 0 else '下降'}<text_tag color={'green' if difference > 0 else 'red'}>{abs(difference * 100):.2f}%</text_tag>，{'高于' if fluctuation > 0 else '低于'}大盘<text_tag color={'green' if fluctuation > 0 else 'red'}>{abs(fluctuation * 100):.2f}%</text_tag>"
+
 
                         self.alert_list.append({
                             'name': sales_name,
@@ -184,7 +190,7 @@ class Monitor4(AirlineMonitorDataHandler):
                             'payer_customer_signedname': payer,
                             'stat_dispaysignedname': customer,
                             'customer_no': item1["CUSTOMER_NO"],
-                            'payer_business_scene': item1["PAYER_BUSINESS_SCENE"],
+                            'payer_business_scene': orig_scene,
                             'sub_content_rich': subcontent,
                         })
 
