df5bed7713fff7d35c643f2936b55b227b3e2617
ideamac <ll@ll.c>
2024-06-03 13:59:18 +0800

    。

--- dbgpt/extra/dag/buildin_awel/monitor/monitor2.py
+++ dbgpt/extra/dag/buildin_awel/monitor/monitor2.py
@@ -1,3 +1,5 @@
+from typing import Dict
+
 from dbgpt.extra.dag.buildin_awel.monitor.airline_monitor_handler import AirlineMonitorDataHandler
 
 
@@ -31,11 +33,13 @@ class Monitor2(AirlineMonitorDataHandler):
         except Exception as e:
             print('监控二开始获取所有付方销售失败')
 
+        d1_datas = self.build_d_n_payer_datas_by_sales_and_signedname("d1")
+
         for payer_sales_name in payer_sales_name_list:
             if payer_sales_name is not None:
-                self.deal_payer_sales_name(payer_sales_name)
+                self.deal_payer_sales_name(d1_datas, payer_sales_name)
 
-    def deal_payer_sales_name(self, payer_sales_name):
+    def deal_payer_sales_name(self, d1_datas, payer_sales_name):
         payer_customer_list = set()
         try:
             print(f'监控二开始获取{payer_sales_name}的付方签约名')
@@ -48,15 +52,34 @@ class Monitor2(AirlineMonitorDataHandler):
             return
 
         for payer_customer in payer_customer_list:
-            self.deal_payer_customer(payer_sales_name, payer_customer)
+            self.deal_payer_customer(d1_datas, payer_sales_name, payer_customer)
 
-    def deal_payer_customer(self, payer_sales_name, payer_customer):
+    def build_d_n_payer_datas_by_sales_and_signedname(self, days_type) -> Dict:
+        """按销售和签约名分组，构造数据"""
+        result: Dict = {}
+        d_n_datas = []
+        if days_type == "d1":
+            d_n_datas = self.monitor2_data.get_data_by_payer_in_monitor2(
+                trx_date=self.d_1_date
+            )
+        print(f'监控二({days_type})构造条数: {len(d_n_datas)}！')
+        for rec in d_n_datas:
+            if rec["PAYER_SALES_NAME"] is None or rec["PAYER_CUSTOMER_SIGNEDNAME"] is None:
+                continue
+            k = str(rec["PAYER_SALES_NAME"]) + '#_#' + str(rec["PAYER_CUSTOMER_SIGNEDNAME"])
+            result[k] = rec
+        return result
+
+    def deal_payer_customer(self, d1_datas: Dict, payer_sales_name, payer_customer):
         try:
             print(f'监控二开始获取{payer_sales_name}的付方签约名为{payer_customer}的数据')
-            d_1_data = \
-                self.monitor2_data.get_data_by_payer_in_monitor2(trx_date=self.d_1_date,
-                                                                 payer_sales_name=payer_sales_name,
-                                                                 payer_customer_signedname=payer_customer)[0]
+            d_1_data = d1_datas[payer_sales_name + "#_#" + payer_customer]
+
+            # d_1_data = self.monitor2_data.get_data_by_payer_in_monitor2(
+            #     trx_date=self.d_1_date,
+            #     payer_sales_name=payer_sales_name,
+            #     payer_customer_signedname=payer_customer
+            # )[0]
 
         except Exception as e:
             print(f'监控二开始获取{payer_sales_name}的付方签约名为{payer_customer}的数据失败')
@@ -85,11 +108,30 @@ class Monitor2(AirlineMonitorDataHandler):
         except Exception as e:
             print(f'监控二开始处理{payer_sales_name}的付方签约名为{payer_customer}的数据失败')
 
+    def build_d_n_stat_datas_by_sales_and_signedname(self, days_type) -> Dict:
+        """按销售和签约名分组，构造数据"""
+        result: Dict = {}
+        d_n_datas = []
+        if days_type == "d1":
+            d_n_datas = self.monitor2_data.get_data_by_stat_in_monitor2(
+                trx_date=self.d_1_date
+            )
+        print(f'监控二({days_type})构造条数: {len(d_n_datas)}！')
+        for rec in d_n_datas:
+            if rec["PAYER_SALES_NAME"] is None or rec["PAYER_CUSTOMER_SIGNEDNAME"] is None:
+                continue
+            k = str(rec["PAYER_SALES_NAME"]) + '#_#' + str(rec["PAYER_CUSTOMER_SIGNEDNAME"])
+            result[k] = rec
+        return result
+
     def run_by_stat(self):
         print('监控二(商户签约名维度)开始执行')
         sales_name_list = set()
         try:
             print('监控二开始获取所有销售')
+
+            d1_datas = self.build_d_n_stat_datas_by_sales_and_signedname("d1")
+
             data = self.monitor2_data.get_data_by_stat_in_monitor2(self.d_1_date)
             for item in data:
                 sales_name_list.add(item['SALES_NAME'])
@@ -97,9 +139,9 @@ class Monitor2(AirlineMonitorDataHandler):
             print('监控二开始获取所有销售失败')
 
         for sales_name in sales_name_list:
-            self.deal_sales_name(sales_name)
+            self.deal_sales_name(d1_datas, sales_name)
 
-    def deal_sales_name(self, sales_name):
+    def deal_sales_name(self, d1_datas, sales_name):
         customer_list = set()
         try:
             print(f'监控二开始获取{sales_name}的商户签约名')
@@ -111,13 +153,18 @@ class Monitor2(AirlineMonitorDataHandler):
             return
 
         for customer in customer_list:
-            self.deal_customer(sales_name, customer)
+            self.deal_customer(d1_datas, sales_name, customer)
 
-    def deal_customer(self, sales_name, customer):
+    def deal_customer(self, d1_datas, sales_name, customer):
         try:
             print(f'监控二开始获取{sales_name}的商户 签约名为{customer}的数据')
-            d_1_data = self.monitor2_data.get_data_by_stat_in_monitor2(trx_date=self.d_1_date, sales_name=sales_name,
-                                                                       stat_dispaysignedname=customer)[0]
+            d_1_data = d1_datas[sales_name + "#_#" + customer]
+
+            # d_1_data = self.monitor2_data.get_data_by_stat_in_monitor2(
+            #     trx_date=self.d_1_date,
+            #     sales_name=sales_name,
+            #     stat_dispaysignedname=customer
+            # )[0]
 
         except Exception as e:
             print(f'监控二开始获取{sales_name}的商户签约名为{customer}的数据失败')
